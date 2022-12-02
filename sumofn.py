1ba1925ab909c6372b6d8ee50b619d222cb5781f
Usman777z <116332448+Usman777z@users.noreply.github.com>
2022-12-02 10:37:58 +0530

    Add files via upload

---
+++ sumofn.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+def sum(n):
+    sum=0
+    for i in range(0,n+1):
+        sum=sum+i
+    print("Sum of n number=",sum)
+    
+    
+n=int(input("Enter a number:"))
+sum(n)
+    
+
+
+# In[ ]:
+
+
+
+
