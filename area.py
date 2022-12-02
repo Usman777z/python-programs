b3c78c21c361eb09336b3065b41682cedd2f061a
Usman777z <116332448+Usman777z@users.noreply.github.com>
2022-12-02 10:43:16 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+a=int(input("Enter first side"))
+b=int(input("Enter second side:"))
+c=int(input("Enter third side:"))
+s=(a+b+c)/2
+ar=s*(s-a)*(s-b)*(s-c)**0.5
+print("Area of triangle is:",ar)
+
+
+# In[ ]:
+
+
+
+
