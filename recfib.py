8bb9ddd238582684f6a9f57307b16f272483bd87
Usman777z <116332448+Usman777z@users.noreply.github.com>
2022-12-16 10:39:10 +0530

    Add files via upload

---
+++ recfib.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+def fib(n):
+    if(n<=1):
+        return n
+    else:
+        return fib(n-1)+fib(n-2)
+        
+n= int(input("Enter a number:"))
+print("fibonacci series :")
+for i in range(n):
+    print(fib(i))
+
+
+
+        
+
+
+# In[ ]:
+
+
+
+
